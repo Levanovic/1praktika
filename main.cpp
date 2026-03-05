1d870cf2953667aec8d6780042eae7d24a022508
Levanovic <lewanowiczpatryk@gmail.com>
2026-03-05 14:00:53 +0200

    Istaisytas variantas. Programa veikia visada, sukurtas 4 pasirinnkimas su isejimu

--- main.cpp
+++ main.cpp
@@ -16,116 +16,133 @@ using namespace std;
      float INR_Bendras   = 104.6918;
      float INR_Pirkti    = 101.3862;
      float INR_Parduoti  = 107.8546;
-     int Pasirinkimas;
-     int Valiuta;
-     // Vartotojo meniu
-     cout << "1 Pasirinkimas - tai valiutos palyginimas" << endl;
-     cout << "2 Pasirinkimas - tai valiutos pirkimas" << endl;
-     cout << "3 Pasirinkimas - tai valiutos pardavimas" << endl;
-     //Vartotojo ivestis: pasirinkimas ir valiuta
-     cout << "Pasirinkite norima veiksma (1-3)" << endl;
-     cin >> Pasirinkimas;
-     cout << "Pasirinkite norima valiuta (1-GBP, 2-USD, 3-INR)" << endl;
-     cin >> Valiuta;
+     while (true) {
+         int Pasirinkimas;
+         int Valiuta;
+         // Vartotojo meniu
+         cout << "                                        " << endl;
+         cout << "1 Pasirinkimas - tai valiutos palyginimas" << endl;
+         cout << "2 Pasirinkimas - tai valiutos pirkimas" << endl;
+         cout << "3 Pasirinkimas - tai valiutos  pardavimas" << endl;
+         cout << "4 Pasirinkimas - tai isejimas is programos" << endl;
+         //Vartotojo ivestis: pasirinkimas ir valiuta
+         cout << "Pasirinkite norima veiksma (1-4)" << endl;
+         cin >> Pasirinkimas;
+         if (Pasirinkimas == 4) {
+             cout << "Isejimas" << endl;
+             break;
+         }else if (Pasirinkimas >= 1 && Pasirinkimas <= 3) {
+             cout << "Pasirinkite norima valiuta (1-GBP, 2-USD, 3-INR)" << endl;
+             cin >> Valiuta;
 
 
-     switch (Pasirinkimas) {
-         case 1:
-             cout << "Valiutos palyginimas: " << endl; // Darysim palyginima, kokia valiuta yra pigesni o kokia brangesni
-             switch (Valiuta) {
+             switch (Pasirinkimas) {
                  case 1:
-                     cout << "Jusu pasirinkta valiuta GBP: " << endl;
-                     if (GBP_Bendras < EUR_Bendras) { // Jeigu GBP yra mazesnis uz EUR, tai Euras bus pigesnis
-                         cout << "Euras yra pigesnis už Svara" << endl;
-                     }else {
-                         cout << "Euras yra brangesnis už Svara" << endl;
-                     }
+                     cout << "Valiutos palyginimas: " << endl; // Darysim palyginima, kokia valiuta yra pigesni o kokia brangesni
+                     switch (Valiuta) {
+                         case 1:
+                             cout << "Jusu pasirinkta valiuta GBP: " << endl;
+                             if (GBP_Bendras < EUR_Bendras) { // Jeigu GBP yra mazesnis uz EUR, tai Euras bus pigesnis
+                                 cout << "Euras yra pigesnis už Svara" << endl;
+                             }else {
+                                 cout << "Euras yra brangesnis už Svara" << endl;
+                             }
 
+                             break;
+                         case 2:
+                             cout << "Jusu pasirinkta valiuta USD: " << endl;
+                             if (USD_Bendras < EUR_Bendras) {
+                                 cout << "Euras yra pigesnis už Doleri" << endl;
+                             }else {
+                                 cout << "Euras yra brangesnis už Doleri" << endl;
+                             }
+                             break;
+                         case 3:
+                             cout << "Jusu pasirinkta valiuta INR: " << endl;
+                             if (INR_Bendras < EUR_Bendras) {
+                                 cout << "Euras yra pigesnis už Rupija" << endl;
+                             }else {
+                                 cout << "Euras yra brangesnis už Rupija" << endl;
+                             }
+                             break;
+                     }
                      break;
                  case 2:
-                     cout << "Jusu pasirinkta valiuta USD: " << endl;
-                     if (USD_Bendras < EUR_Bendras) {
-                         cout << "Euras yra pigesnis už Doleri" << endl;
-                     }else {
-                         cout << "Euras yra brangesnis už Doleri" << endl;
+                     cout << "Valiutos pirkimas: " << endl; //Pirksim (konvertuosim eur i pasirinkta valiuta) valiuta naudojant formula - ? = Eur * norima valiuta
+                     switch (Valiuta) {
+                         case 1: {
+                             cout << "Jus norit nupirkti GBP. Kiek turit eurus?: " << endl;
+                             float Eur;
+                             cin >> Eur;
+                             float gauti_GBP = Eur * GBP_Pirkti;//formula
+                             cout << fixed << setprecision(2) << endl; // fixed << setprecision (2) skirta tam, kad apvalinti skaicius iki simtuju
+                             cout << "Už " << Eur << " eur jus gausite " << gauti_GBP << " GBP." << endl;
+                             break;
+                         }
+                         case 2: {
+                             cout << "Jus norit nupirkti USD. Kiek turit eurus?: " << endl;
+                             float Eur;
+                             cin >> Eur;
+                             float gauti_USD = Eur * USD_Pirkti; //formula
+                             cout << fixed << setprecision(2) << endl;
+                             cout << "Už " << Eur << " eur jus gausite " << gauti_USD << " USD." << endl;
+                             break;
+                         }
+                         case 3: {
+                             cout << "Jus norit nupirkti INR. Kiek turit eurus?: " << endl;
+                             float Eur;
+                             cin >> Eur;
+                             float gauti_INR = Eur * INR_Pirkti;//formula
+                             cout << fixed << setprecision(2) << endl;
+                             cout << "Už " << Eur << " eur jus gausite " << gauti_INR << " INR." << endl;
+                             break;
+                         }
+
                      }
                      break;
                  case 3:
-                     cout << "Jusu pasirinkta valiuta INR: " << endl;
-                     if (INR_Bendras < EUR_Bendras) {
-                         cout << "Euras yra pigesnis už Rupija" << endl;
-                     }else {
-                         cout << "Euras yra brangesnis už Rupija" << endl;
+                     cout << "Valiutos pardavimas: " << endl;
+                     switch (Valiuta) {
+                         case 1: {
+                             cout << "Kiek GBP norite parduot?" << endl;
+                             float Kiekis;
+                             cin >> Kiekis;
+                             float gauti_EUR = Kiekis / GBP_Parduoti;
+                             cout << fixed << setprecision(2) << endl;
+                             cout << "Už " << Kiekis << " GBP, jus gausite " << gauti_EUR << " EUR." << endl;
+                             break;
+                         }
+                         case 2: {
+                             cout << "Kiek USD norite parduot?" << endl;
+                             float Kiekis;
+                             cin >> Kiekis;
+                             float gauti_EUR = Kiekis / USD_Parduoti;
+                             cout << fixed << setprecision(2) << endl;
+                             cout << "Už " << Kiekis << " USD, jus gausite " << gauti_EUR << " EUR." << endl;
+                             break;
+                         }
+                         case 3: {
+                             cout << "Kiek INR norite parduot?" << endl;
+                             float Kiekis;
+                             cin >> Kiekis;
+                             float gauti_EUR = Kiekis / INR_Parduoti;
+                             cout << fixed << setprecision(2) << endl;
+                             cout << "Už " << Kiekis << " INR, jus gausite " << gauti_EUR << " EUR." << endl;
+                             break;
+                         }
                      }
                      break;
-             }
-             break;
-         case 2:
-             cout << "Valiutos pirkimas: " << endl; //Pirksim (konvertuosim eur i pasirinkta valiuta) valiuta naudojant formula - ? = Eur * norima valiuta
-             switch (Valiuta) {
-                 case 1: {
-                     cout << "Jus norit nupirkti GBP. Kiek turit eurus?: " << endl;
-                     float Eur;
-                     cin >> Eur;
-                     float gauti_GBP = Eur * GBP_Pirkti;//formula
-                     cout << fixed << setprecision(2) << endl; // fixed << setprecision (2) skirta tam, kad apvalinti skaicius iki simtuju
-                     cout << "Už " << Eur << " eur jus gausite " << gauti_GBP << " GBP." << endl;
-                     break;
-                 }
-                 case 2: {
-                     cout << "Jus norit nupirkti USD. Kiek turit eurus?: " << endl;
-                     float Eur;
-                     cin >> Eur;
-                     float gauti_USD = Eur * USD_Pirkti; //formula
-                     cout << fixed << setprecision(2) << endl;
-                     cout << "Už " << Eur << " eur jus gausite " << gauti_USD << " USD." << endl;
-                     break;
-                 }
-                 case 3: {
-                     cout << "Jus norit nupirkti INR. Kiek turit eurus?: " << endl;
-                     float Eur;
-                     cin >> Eur;
-                     float gauti_INR = Eur * INR_Pirkti;//formula
-                     cout << fixed << setprecision(2) << endl;
-                     cout << "Už " << Eur << " eur jus gausite " << gauti_INR << " INR." << endl;
-                     break;
-                 }
+
 
              }
-             break;
-         case 3:
-             cout << "Valiutos pardavimas: " << endl;
-             switch (Valiuta) {
-                 case 1: {
-                     cout << "Kiek GBP norite parduot?" << endl;
-                     float Kiekis;
-                     cin >> Kiekis;
-                     float gauti_EUR = Kiekis * GBP_Parduoti;
-                     cout << fixed << setprecision(2) << endl;
-                     cout << "Už " << Kiekis << " GBP, jus gausite " << gauti_EUR << " EUR." << endl;
-                     break;
-                 }
-                 case 2: {
-                     cout << "Kiek USD norite parduot?" << endl;
-                     float Kiekis;
-                     cin >> Kiekis;
-                     float gauti_EUR = Kiekis * USD_Parduoti;
-                     cout << fixed << setprecision(2) << endl;
-                     cout << "Už " << Kiekis << " USD, jus gausite " << gauti_EUR << " EUR." << endl;
-                     break;
-                 }
-                 case 3: {
-                     cout << "Kiek INR norite parduot?" << endl;
-                     float Kiekis;
-                     cin >> Kiekis;
-                     float gauti_EUR = Kiekis * INR_Parduoti;
-                     cout << fixed << setprecision(2) << endl;
-                     cout << "Už " << Kiekis << " INR, jus gausite " << gauti_EUR << " EUR." << endl;
-                     break;
-                 }
-             }
-         break;
-     }
+         }else {
+             cout << "Blogas pasirinkimas" << endl;
+         }
+        }
+
+
+
+
 
   return 0;
   }
